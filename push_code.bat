35b62b5b891bb9561058f6edcbcdd32d7b44b8cd
Mẫn Henry <tvm19624@gmail.com>
2026-08-26 22:46:24 +0700

    Don dep code sach se, toi uu hoa tai nguyen va he thong

--- push_code.bat
+++ push_code.bat
@@ -4,7 +4,7 @@ git add .
 git commit -m "Don dep code sach se, toi uu hoa tai nguyen va he thong"
 git branch -M main
 git remote remove origin 2>nul
-git remote add origin https://github.com/Man-Henry/FPT-TELECOM.git
+git remote add origin https://github.com/Man-Henry/FPT_TELECOM.git
 echo Dang day code len GitHub...
 git push -u -f origin main
 echo Hoan thanh day code!
